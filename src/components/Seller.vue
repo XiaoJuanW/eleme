2b5c0f13d04c8a0c1e301f6521249179711500cb
Joy <joy@JoydeMacBook-Pro.local>
2018-09-10 13:52:08 +0800

    first commit

---
+++ src/components/Seller.vue
@@ -0,0 +1,9 @@
+<template>
+<div>商家</div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="stylus">
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Goods from '@/components/Goods';
+import Ratings from '@/components/Ratings';
+import Seller from '@/components/Seller';
+
+Vue.use(Router)
+
+export default new Router({
+  linkActiveClass: 'active',
+  routes: [{
+    path: '/',
+    redirect: '/goods'
+  }, {
+    path: '/goods',
+    component: Goods
+  }, {
+    path: '/ratings',
+    component: Ratings
+  }, {
+    path: '/seller',
+    component: Seller
+  }]
+})
