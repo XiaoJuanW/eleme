676878ddf810c0f476042a33c228c1d9f9dbdafc
Joy <joy@JoydeMacBook-Pro.local>
2018-09-10 17:35:55 +0800

    购物车-2

--- src/components/ShopCart.vue
+++ src/components/ShopCart.vue
@@ -3,15 +3,16 @@
     <div class="content">
       <div class="content-left">
         <div class="logo-wrapper">
-          <div class="logo">
-            <i class="icon-shopping_cart"></i>
+          <div class="logo" :class="{highlight: totalPrice > 0}">
+            <i class="icon-shopping_cart" :class="{highlight: totalPrice > 0}"></i>
           </div>
+          <div v-show="totalCount > 0" class="num">{{totalCount}}</div>
         </div>
-        <div class="price">300</div>
+        <div class="price" :class="{highlight: totalPrice > 0}">¥{{totalPrice}}</div>
         <div class="desc">¥另需配送费{{deliveryPrice}}元</div>
       </div>
       <div class="content-right">
-        <div class="pay">¥{{minPrice}}元起送</div>
+        <div class="pay" :class="payClass">{{payDesc}}</div>
       </div>
     </div>
   </div>
@@ -19,6 +20,17 @@
 <script>
 export default {
 	props: {
+		selectFoods: {
+			type: Array, // 如果是array或obj， default需要是函数
+			default() {
+				return [
+					{
+						price: 10,
+						count: 1
+					}
+				];
+			}
+		},
 		deliveryPrice: {
 			tytype: Object,
 			default: 0
@@ -27,6 +39,40 @@ export default {
 			tytype: Object,
 			default: 0
 		}
+	},
+	computed: {
+		totalPrice() {
+			let total = 0;
+			this.selectFoods.forEach(food => {
+				total += food.price * food.count;
+			});
+			return total;
+		},
+		totalCount() {
+			let count = 0;
+			this.selectFoods.forEach(food => {
+				count += food.count;
+			});
+			return count;
+		},
+		payDesc() {
+			if (this.totalPrice === 0) {
+				// ``ES6语法
+				return `¥${this.minPrice}元起送`;
+			} else if (this.totalPrice < this.minPrice) {
+				let diff = this.minPrice - this.totalPrice;
+				return `还差¥${diff}元起送`;
+			} else {
+				return "去结算";
+			}
+    },
+    payClass() {
+      if(this.totalPrice >= this.minPrice) {
+        return 'enough';
+      }else {
+        return 'bot-enough';
+      }
+    }
 	}
 };
 </script>
@@ -70,11 +116,35 @@ export default {
           text-align: center;
           background: #2b343c;
 
+          &.highlight {
+            background: rgb(0, 160, 220);
+          }
+
           .icon-shopping_cart {
             line-height: 44px;
             font-size: 24px;
             color: #80858a;
+
+            &.highlight {
+              color: #fff;
+            }
           }
+        }
+
+        .num {
+          position: absolute;
+          top: 0;
+          right: 0;
+          width: 24px;
+          height: 16px;
+          line-height: 16px;
+          text-align: center;
+          border-radius: 16px;
+          font-size: 9px;
+          font-weight: 700;
+          color: #fff;
+          background: rgb(240, 20, 20);
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
         }
       }
 
@@ -88,6 +158,10 @@ export default {
         border-right: 1px soloid rgba(255, 255, 255, 0.1);
         font-size: 16px;
         font-weight: 700;
+
+        &.highlight {
+          color: #fff;
+        }
       }
 
       .desc {
@@ -110,6 +184,16 @@ export default {
         font-size: 12px;
         font-weight: 700;
         background: #2b333b;
+
+        &.not-enough {
+          background: #2b333b;
+          color: rgba(255, 255, 255, 0.4);
+        }
+
+        &.enough {
+          background: #00b43c;
+          color: #fff;
+        }
       }
     }
   }
