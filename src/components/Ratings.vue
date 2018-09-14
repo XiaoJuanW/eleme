3cf777234b61d57fbeb741fa8d3fb79d76673123
Joy <827270377@qq.com>
2018-09-14 17:16:38 +0800

    Ratings Component part overview

--- src/components/Ratings.vue
+++ src/components/Ratings.vue
@@ -1,9 +1,148 @@
 <template>
-<div>评论</div>
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家{{seller.rankRate}}</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品态度</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-export default {};
+import BScroll from "better-scroll";
+import Star from "@/components/Star.vue";
+export default {
+	props: {
+		seller: {
+			type: Object
+		}
+	},
+	data() {
+		return {};
+	},
+	components: {
+		Star
+	}
+};
 </script>
 <style lang="stylus">
+.ratings {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  width: 100%;
+  overflow: hidden;
+
+  .overview {
+    display: flex;
+    padding: 18px 0;
+
+    .overview-left {
+      flex: 0 0 137px;
+      padding: 6px 0;
+      width: 137px;
+      text-align: center;
+      border-right: 1px solid rgba(7, 17, 27, 0.1);
+
+      // 适配小屏幕
+      @media only screen and (max-width: 320px) {
+        flex: 0 0 120px;
+        width: 120px;
+      }
+
+      .score {
+        margin-bottom: 6px;
+        line-height: 28px;
+        font-size: 24px;
+        color: rgb(255, 153, 0);
+      }
+
+      .title {
+        margin-bottom: 8px;
+        line-height: 12px;
+        font-size: 12px;
+        color: rgb(7, 17, 27);
+      }
+
+      .rank {
+        font-size: 10px;
+        color: rgb(147, 153, 159);
+      }
+    }
+
+    .overview-right {
+      flex: 1;
+      padding: 6px 0 6px 24px;
+
+      // 适配小屏幕
+      @media only screen and (max-width: 320px) {
+        padding-left: 6px;
+      }
+
+      .score-wrapper {
+        line-height: 18px;
+        margin-bottom: 8px;
+        font-size: 0;
+
+        .title {
+          display: inline-block;
+          vertical-align: top;
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(7, 17, 27);
+        }
+
+        .star {
+          display: inline-block;
+          vertical-align: top;
+          margin: 0 12px;
+        }
+
+        .score {
+          display: inline-block;
+          vertical-align: top;
+          font-size: 12px;
+          color: rgb(255, 153, 0);
+        }
+      }
+
+      .delivery-wrapper {
+        font-size: 0;
+
+        .title {
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(7, 17, 27);
+        }
+
+        .delivery {
+          margin-left: 12px;
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(147, 153, 159);
+        }
+      }
+    }
+  }
+}
 </style>
 
